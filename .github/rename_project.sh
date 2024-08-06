bca2a87d92023b4a1d7503fce0aeed5fba98be31
pgarrett-scripps <pgarrett-scripps@users.noreply.github.com>
2024-08-06 01:07:37 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="pgarrett-scripps"
+original_name="timscandidatetracker"
+original_urlname="TimsCandidateTracker"
+original_description="Awesome timscandidatetracker created by pgarrett-scripps"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv timscandidatetracker $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
